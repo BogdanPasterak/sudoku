0ac81c02c8a40f6cdece0db386efa168533987fb
bogdanpasterak <bogdanpasterak@gmail.com>
2020-07-11 19:31:38 +0100

    some getChar

--- algorythms/brute-force.js
+++ algorythms/brute-force.js
@@ -1,44 +1,46 @@
 const Queue = require("../models/queue");
 const Veryfi = require("../functions/veryfication");
+const readline = require('readline');
+
 
 module.exports = function(board) {
-    const queue = new Queue();
+    // const queue = new Queue();
     const veryfi = Veryfi();
-    const list = new Array();
+    // const list = new Array();
 
-    let ans = next(veryfi, board, 0, 0, 1);
+    let ans = next(veryfi, board, 0, 0);
 
-    return "now " + ans.x + " " + ans.y;
+    return ans;
 }
 
-function next(veryfi, board, x, y, v) {
-    let ans = empty(board, x, y);
-    if (ans) {
-        if (x !== ans.x || y !== ans.y) {
-            v = 1;
-        }
-        board[ans.x][ans.y] = v;
-        if (!veryfi.field(board, ans.x, ans.y)) {
-            v++;
-            board[ans.x][ans.y] = 0;
-            if (v > 9) return false;
-        }
-        let back = next(veryfi, board, ans.x, ans.y, v);
-        if (back) {
-            return true;
-        } else {
-            if (x === ans.x && y === ans.y) {
-                return false;
+function next(veryfi, board, x, y) {
+
+    var query = require('cli-interact').getChar;
+    var answer = query('?', 'abcdez\n');
+    console.log('you answered:', answer);
+    // const ans = askQuestion("Are you sure you want to deploy to PRODUCTION? ");
+
+    // ans.then(a => console.log("A", a));
+
+    let empt = empty(board, x, y);
+    if (empt) {
+        for (let val = 1; val <= 9; val++) {
+            board[empt.x][empt.y] = val;
+            if (veryfi.field(board, empt.x, empt.y)) {
+                let back = next(veryfi, board, empt.x, empt.y);
+                if (!back) {
+                    board[empt.x][empt.y] = 0;
+                } else {
+                    return "Fill !!";
+                }
             } else {
-                console.log("Bogdan");
-                v = board[x][y] + 1;
-                board[x][y] = 0;
-                if (v > 9) return false;
-                return true;
+                board[empt.x][empt.y] = 0;
             }
         }
+        return false;
+    } else {
+        return "Fill !!";
     }
-    return false;
 }
 
 function empty(board, x, y) {
@@ -54,3 +56,28 @@ function empty(board, x, y) {
     } while (y < 9);
     return false;
 }
+
+// function askQuestion(query) {
+//     const rl = readline.createInterface({
+//         input: process.stdin,
+//         output: process.stdout,
+//     });
+
+//     return new Promise(resolve => rl.question(query, ans => {
+//         rl.close();
+//         resolve(ans);
+//     }))
+// }
+
+// const stdin = process.stdin;
+// stdin.setRawMode(true);
+// stdin.resume();
+// stdin.setEncoding('utf8');
+// stdin.on('data', function(key) {
+//     // ctrl-c ( end of text )
+//     if (key === '\u0003') {
+//         process.exit();
+//     }
+//     // write the key to stdout all normal like
+//     process.stdout.write(key);
+// });
